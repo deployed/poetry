3d1f52e5c46ab7dd198f60c059b1517c6ee6e0ec
Dominik <dominik.godek@gmail.com>
2023-12-09 13:09:20 +0100

    API

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,51 @@
+import AuthorInfo from "@/components/AuthorInfo/AuthorInfo";
 import { Link } from "expo-router";
-import React from "react";
-import { View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, FlatList } from "react-native";
+import {AuthorInfoProps, PoemInfo} from "@/types/types"
+
 
 const App = () => {
+  const [getDatabase, setDatabase] = useState<PoemInfo[]>([])
+  const fetchPoems = () => {
+    return fetch('https://poetrydb.org/author,title/all;all')
+      .then(response => response.json())
+      .then(json => {
+        setDatabase(json)
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+  useEffect(() => {fetchPoems(); console.log(getDatabase) }, [] )
+  console.log(getDatabase)
   return (
     <View>
-      <Link
+      <FlatList 
+      ItemSeparatorComponent={() =>
+        <View
+  style={{
+    borderBottomColor: 'black',
+    borderBottomWidth: 2,
+  }}
+/>
+      }
+data={getDatabase}
+renderItem={({item}) => <AuthorInfo title={item.title} author={item.author} linecount={item.linecount}  lines={item.lines} /> }
+/>
+      {/* <Link
         href={{
           pathname: "/poem/123",
         }}
       >
-        View user
-      </Link>
+        View sddffdf
+      </Link> */}
     </View>
   );
 };
 
 export default App;
+function UseEffect(arg0: () => void, arg1: never[]) {
+  throw new Error("Function not implemented.");
+}
+
---
+++ src/types/types.ts
@@ -0,0 +1,3 @@
+
+export interface AuthorInfoProps {author: string, linecount: string, lines: string[], title: string}
+export type PoemInfo = {title: string, author: string, lines: string[], linecount: string}
